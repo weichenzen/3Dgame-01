151be65d9640876df2bf8857c08a3114da47b0a2
weichenzen <111750507+weichenzen@users.noreply.github.com>
2022-12-21 21:30:28 +0800

    3Dgame-01

---
+++ Assets/Scripts/LearnArray.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace MyNamespace
+{
+    /// <summary>
+    /// 學習陣列 Array
+    /// </summary>
+	public class LearnArray : MonoBehaviour
+    {
+        public int expLv1 = 100;
+        public int expLv2 = 10000;
+        public int expLv3 = 1000000;
+
+        // 陣列：儲存多筆相同類型的資料
+        // 語法：
+        // 修飾詞 資料類型[] 欄位名稱 指定 預設值;
+        public int[] expLv;
+
+        // 給陣列資料
+        public string[] weapons = new string[3];
+
+        // 任務資料
+        // 1.過關
+        // 2.過關
+        // 3.未過關
+        public bool[] missions = { true, true, false };
+
+        public string[] pets = { "皮卡丘", "小智", "比雕" };
+
+		private void Awake()
+		{
+            // 存取陣列資料
+            // 取得Get
+            // 陣列名稱[編號]
+            print(pets[2]);
+
+            // 存放 Set
+            // 陣列名稱[編號] 指定 值；
+
+		}
+	}
+}
