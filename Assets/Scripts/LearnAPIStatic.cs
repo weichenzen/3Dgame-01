276e8a519e80f9095e0fe64d2f57b06b89687063
weichenzen <111750507+weichenzen@users.noreply.github.com>
2023-01-09 21:28:29 +0800

    3Dgame-01

---
+++ Assets/Scripts/LearnAPIStatic.cs
@@ -0,0 +1,41 @@
+﻿using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+
+namespace KID
+{
+    /// <summary>
+    /// 學習API：靜態 Static
+    /// </summary>
+	public class LearnAPIStatic : MonoBehaviour
+    {
+		private void Awake()
+		{
+			// 靜態屬性 Static Properties
+			// 1. 取得
+			// 語法：
+			// 類別.靜態屬性名稱
+			print("隨機值：" + Random.value);
+			print("當前指標能見度" + Cursor.visible);
+
+			// 2. 設定
+			// 語法：
+			// 類別.靜態屬性名稱 指定 值;
+			Cursor.visible = false;          // 隱藏滑鼠
+			Screen.fullScreen = true;        // 設定為全螢幕
+
+			// 唯讀屬性不能設定 (Read Only)
+			// Random.value = 1.234f;
+
+			// 靜態方法 Static Method
+			// 語法：
+			// 類別.靜態方法名稱(對應的引數);
+			print(Mathf.Abs(-321.5f));       // 數學.絕對值(數值);
+
+			// Random.RandomRange(100f, 200f);    // 已過時的 API 建議不要用
+
+			float random = Random.Range(100f, 200f);
+			print($"<color=yellow>隨機值介於 100 與 200：{random}</Color>");
+		}
+	}
+}
--- Assets/Scripts/LearnLoop.cs
+++ Assets/Scripts/LearnLoop.cs
@@ -7,6 +7,8 @@ namespace Tom
     /// </summary>
 	public class LearnLoop : MonoBehaviour
     {
+		public string[] players = { "小智", "小剛", "小霞", "小光", "皮卡丘" };
+
 		private void Awake()
 		{
 			// while 語法：
@@ -18,6 +20,17 @@ namespace Tom
 			{
 				print($"<color=#119933>while 迴圈： { number } </color>");
 				number++;
+			}
+
+			// for (初始值；布林值；迴圈結束會執行的程式) { 程式，當布林值為 true 才會持續執行 }
+			for (int i = 0; i < 5; i++)
+			{
+				print($"<color=#ff3333>for 迴圈: { i } </color>");
+			}
+
+			for (int i = 0; i < players.Length; i++)
+			{
+				print($"<color=#3333ff>玩家名稱： { players[i] } </color>");
 			}
 		}
 	}
